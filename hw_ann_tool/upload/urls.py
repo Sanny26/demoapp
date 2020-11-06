9c308521f6ec4fe9acecd7ddd4b17eda5c220acd
Sanny26 <santhoshiin26@gmail.com>
2020-11-06 17:02:32 +0530

    update

--- hw_ann_tool/upload/urls.py
+++ hw_ann_tool/upload/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('upload', views.upload, name='upload'),
+    # path('review', views.review, name='review'),
+    re_path(r'^review/(?P<para_id>\d+)/$', views.review, name='review'),
 ]
--- hw_ann_tool/upload/views.py
+++ hw_ann_tool/upload/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 
 from .forms import ImUpForm, DetailsForm
+from hw_ann_tool.settings import MEDIA_URL,MEDIA_ROOT
 
 import random
 import os
+import io
+import PIL.Image as Image
 
 def upload(request):
 	page_template = "upload.html"
@@ -11,10 +14,50 @@ def upload(request):
 	lang = request.session['lang']
 	dirpath = f'media/{lang}_paras/'
 	files = os.listdir(dirpath)
+	upload_prefix = f'{MEDIA_ROOT}/uploads/{lang}_paras/'
 
-	# text = "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:\
-	# 		Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:"
-	nfile = f'{dirpath}{random.choice(files)}'
+	if 'para_id' not in request.session:
+		para_id = random.choice(files)[:-4]
+	else:
+		para_id = request.session['para_id']
+		del request.session['para_id']
+	nfile = f'{dirpath}{para_id}.txt'
+	print(f'upload {para_id}')
+	text = open(nfile).read()
+	context['text'] = text		
+	
+	if request.method == 'POST':
+		form = ImUpForm(request.POST, request.FILES)
+		if form.is_valid():
+			fobj = request.FILES['hwform']
+			jpeg_array = bytearray(fobj.read())
+			image = Image.open(io.BytesIO(jpeg_array))
+			image = image.convert('RGB')
+			savepath = f'{upload_prefix}{para_id}.jpg'
+			# need to create db model to link filename, email, paragraph id(rid)
+			request.session['para_id'] = para_id
+			image.save(savepath)
+			return redirect('review', para_id=para_id)
+	else:
+		print(f'upload get request{para_id}')
+		form = ImUpForm()
+
+	context['form'] = form
+	return render(request, page_template, context) 
+
+
+def review(request, para_id):
+	page_template = "review.html"
+	context = {}
+	lang = request.session['lang']
+	dirpath = f'media/{lang}_paras/'
+	files = os.listdir(dirpath)
+	upload_prefix = f'{MEDIA_ROOT}/uploads/{lang}_paras/'
+	display_prefix = f'{MEDIA_URL}/uploads/{lang}_paras/'
+
+	# para_id = request.session['para_id']
+	nfile = f'{dirpath}{para_id}.txt'
+	print(f'upload {para_id}')
 	text = open(nfile).read()
 	context['text'] = text	
 	
@@ -22,8 +65,15 @@ def upload(request):
 		form = ImUpForm(request.POST, request.FILES)
 		if form.is_valid():
 			fobj = request.FILES['hwform']
-			return redirect('home')
+			jpeg_array = bytearray(fobj.read())
+			image = Image.open(io.BytesIO(jpeg_array))
+			image = image.convert('RGB')
+			savepath = f'{upload_prefix}{para_id}.jpg'
+			# need to create db model to link filename, email, paragraph id(rid)
+			image.save(savepath)
+			return redirect('review', para_id=para_id)
 	else:
+		context['im_path'] =  f'{display_prefix}{para_id[:-4]}.jpg'
 		form = ImUpForm()
 
 	context['form'] = form
@@ -40,6 +90,7 @@ def home(request):
 			lang = form.cleaned_data.get("lang")
 			print('CHosen lang', lang)
 			request.session['lang'] = lang
+			request.session['email'] = form.cleaned_data.get("email")
 			return redirect('upload')
 
 
